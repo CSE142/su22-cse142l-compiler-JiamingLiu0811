036137ef02a4b1540f8759bceb30f2e9059a6b40
Jiaming Liu <jil244@ucsd.edu>
2022-08-12 19:41:28 -0700

    Solution to the lab.

--- sum.cpp
+++ sum.cpp
@@ -6,7 +6,6 @@
 #include<algorithm>
 #include<cstdint>
 #include<unordered_set>
-#include<map>
 #include"function_map.hpp"
 
 
@@ -34,6 +33,7 @@ bool binarySearh(uint64_t a, uint64_t *search_space,  uint32_t l, uint32_t r){
 
 uint64_t sum_of_locations_solution(uint64_t *search_space, uint32_t search_space_size, uint64_t* queries, uint32_t query_count)
 {
+
         std::sort(search_space, &search_space[search_space_size]);
         uint32_t r = 0;
         for(uint32_t i = 0; i < query_count; i++) {
@@ -51,22 +51,6 @@ uint64_t sum_of_locations_solution(uint64_t *search_space, uint32_t search_space
 	return r;
 }
 
-
-/*{
-    std::unordered_set<uint64_t> mySet;
-
-    for(uint32_t j = 0; j < search_space_size; j++){
-        mySet.insert(search_space[j]);
-    }
-    uint32_t r = 0;
-	for(uint32_t i = 0; i < query_count; i++) {
-        std::unordered_set<uint64_t>::const_iterator it = mySet.find (queries[i]);
-        if ( it != mySet.end() ){
-            r += i;
-        }
-	}
-	return r;
-}*/
 
 
 
